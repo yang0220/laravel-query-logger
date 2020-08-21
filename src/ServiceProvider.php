cc806dbe0243ccbd97f25336d748c5cb2dc12ece
yangxiaoqi <1522866027@qq.com>
2020-08-21 18:59:40 +0800

    secc

---
+++ src/ServiceProvider.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the overtrue/laravel-query-logger.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Overtrue\LaravelQueryLogger;
+
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        if (!$this->app['config']->get('logging.query.enabled', false)) {
+            return;
+        }
+
+        DB::listen(function (QueryExecuted $query) {
+            if ($query->time < $this->app['config']->get('logging.query.slower_than', 0)) {
+                return;
+            }
+
+            $sqlWithPlaceholders = str_replace(['%', '?'], ['%%', '%s'], $query->sql);
+
+            $bindings = $query->connection->prepareBindings($query->bindings);
+            $pdo = $query->connection->getPdo();
+            $realSql = $sqlWithPlaceholders;
+            $duration = $this->formatDuration($query->time / 1000);
+
+            if (count($bindings) > 0) {
+                $realSql = vsprintf($sqlWithPlaceholders, array_map([$pdo, 'quote'], $bindings));
+            }
+            Log::debug(sprintf('[%s] [%s] %s | %s: %s', $query->connection->getDatabaseName(), $duration, $realSql, request()->method(), request()->getRequestUri()));
+        });
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Format duration.
+     *
+     * @param float $seconds
+     *
+     * @return string
+     */
+    private function formatDuration($seconds)
+    {
+        if ($seconds < 0.001) {
+            return round($seconds * 1000000).'μs';
+        } elseif ($seconds < 1) {
+            return round($seconds * 1000, 2).'ms';
+        }
+
+        return round($seconds, 2).'s';
+    }
+}
